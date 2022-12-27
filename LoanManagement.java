b3e868c39aa0d52240d926bb7deaec72d520a434
swethadasr <121454643+swethadasr@users.noreply.github.com>
2022-12-27 17:51:45 +0530

    Update LoanManagement.java

--- LoanManagement.java
+++ LoanManagement.java
@@ -6,7 +6,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Scanner;
 
-class LoanManagementSystem
+public class LoanManagement
 {	
 	static double PercentRatePerAnum = 12;
 	
